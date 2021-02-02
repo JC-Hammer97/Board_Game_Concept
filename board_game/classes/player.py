9e0783d95dc3e3dfc3dd068c23fcca0d245f42fc
JC-Hammer97 <76877248+JC-Hammer97@users.noreply.github.com>
2021-02-02 21:08:54 +0000

    Add files via upload

---
+++ board_game/classes/player.py
@@ -0,0 +1,5 @@
+# This class will define the player and what they can do
+
+class Player:
+    def __init__(self):
+        self.actions = ["attack?", "role dice?", "explore?"]
--- board_game/game.py
+++ board_game/game.py
@@ -1,7 +1,11 @@
-
+# What can we do in this game, how does it begin
 
 game_on = True
 
 while game_on:
 
     print("game start")
+    print("you find yourself.... (leading an empire, fighting monsters?)")
+
+    choice = input("Choose action:")
+
