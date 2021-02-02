ba20e607a8264c1bf6d3545b3c112b53eb021530
JC-Hammer97 <76877248+JC-Hammer97@users.noreply.github.com>
2021-02-02 21:36:20 +0000

    Add files via upload

--- board_game/classes/player.py
+++ board_game/classes/player.py
@@ -3,3 +3,10 @@
 class Player:
     def __init__(self):
         self.actions = ["attack?", "role dice?", "explore?"]
+
+    def choose_action(self):
+        i = 1
+        print("ACTIONS")
+        for item in self.actions:
+            print(str(i)+".", item)
+            i += 1
--- board_game/game.py
+++ board_game/game.py
@@ -1,11 +1,25 @@
+from classes.player import Player
+
+
 # What can we do in this game, how does it begin
 
 game_on = True
+
+player1 = Player()
 
 while game_on:
 
     print("game start")
     print("you find yourself.... (leading an empire, fighting monsters?)")
 
+    player1.choose_action()
     choice = input("Choose action:")
 
+    if choice == 1:
+        ...
+
+    elif choice == 2:
+        ...
+
+    elif choice == 3:
+        ...
